8c9fca25dad1095cbd318e222f02d8d85e404b02
aryan012857 <aryansandhu644@gmail.com>
2026-03-23 13:19:07 +0530

    Update check_models.py

--- check_models.py
+++ check_models.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import os
 
 # 1. Put your API Key here directly for this test
-api_key = "AIzaSyAF9lc86ygtTeKKRqrBRaMAxFS9ktANH0A" 
+api_key = "AIzaSyA5rkwTIzYDwrlrReGEJ4LVRrzRmSrjCrw" 
 
 genai.configure(api_key=api_key)
 
